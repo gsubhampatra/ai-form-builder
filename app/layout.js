ad2ce730599da5dcf21288235d7f25bcec914e4d
gsubhampatra <gpb021@nist.edu>
2024-06-14 15:59:34 +0530

    ogimg added

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,16 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "./_components/Header";
 import { Toaster } from "@/components/ui/sonner";
-
+import IMG from "../public/ogimg.jpg";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "AI Form Builder",
   description:
     "Automate, Customize, and Streamline Your Form Building Process with Cutting-Edge AI Technology",
+  openGraph: {
+    images: [IMG, "https://ibb.co/pRBxjgK"],
+  },
 };
 
 export default function RootLayout({ children }) {
